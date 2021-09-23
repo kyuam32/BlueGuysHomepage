785b55d03594a2bb0b82c06af3ccf69afabcc952
lnk0302 <lnk0302@users.noreply.github.com>
2021-09-23 12:37:49 +0900

    :fire: 네이버 지도 api 테스트

--- srcs/components/SubMain/SubMain.js
+++ srcs/components/SubMain/SubMain.js
@@ -3,27 +3,31 @@ import Profile from "../about/Profile";
 import Organization from "../about/Orgaization";
 import ProductDrPODI from "../about/ProductDrPODI";
 import ProductShuFooter from "../about/ProductShuFooter";
-
+import Map from "../about/Map";
+import {useEffect, useState} from "react";
 import dynamic from 'next/dynamic'
 
+// const MapComponent = dynamic(() => import('../about/Map'));
 
-const DynamicMap = dynamic(import('../about/Map'),
-	{
-		loading: () => (<div>Map Loading</div>),
-		ssr: false,
-	},
-);
 
+// 	loading: () => (<div>Map Loading</div>),
+// 		ssr: false,},
 
 const Wrapper = styled.div`
 `
 
 const SubMain = ({curr}) => {
+
+	// const [load, setLoad] = useState(false);
+	// useEffect(() => {
+	// 	setLoad(true);
+	// }, []);
+
 	return (
 		<Wrapper>
 			{ curr.page === "Profile" &&<Profile/>}
 			{ curr.page === "Organization" &&<Organization/>}
-			{ curr.page === "Map" &&<DynamicMap/>}
+			{ curr.page === "Map" && load && <Map/>}
 			{ curr.page === "ProductDrPODI" &&<ProductDrPODI/>}
 			{ curr.page === "ProductShuFooter" &&<ProductShuFooter/>}
 		</Wrapper>
--- srcs/components/about/Map.js
+++ srcs/components/about/Map.js
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps'
-// import {useEffect, useState} from "react";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -12,41 +10,45 @@ const MapContainer = styled.div`
   height: 60rem;
 `
 
-function NaverMapAPI(){
-	const navermaps = window.naver.maps;
-	return (
-		<NaverMap
-			mapDivId={"maps-getting-started-uncontrolled"}
-			style={{
-				width : "100%",
-				height: "100%"
-			}}
-			defaultCenter={{lat: 37.551229, lng: 126.988205}}
-			defaltzoom={13}
-		>
-			<Marker
-				key={1}
-				position={new navermaps.LatLng(37.551229, 126.988205)}
-				animation={1}
-				// onClick={() => {alert('주소 : 서울 강남구 선릉로 627');}}
-			/>
-		</NaverMap>
-	);
-}
-
 const Map = ()=>{
 	const API_KEY = process.env.NEXT_PUBLIC_API_CLIENT_ID
+	let client;
+	if (typeof window !== 'undefined') {
+		client = require('react-naver-maps')
+	}
+
+	function NaverMapAPI() {
+		const navermaps = window.naver.maps;
+		return (
+			<client.NaverMap
+				mapDivId={"maps-getting-started-uncontrolled"}
+				style={{
+					width : "100%",
+					height: "100%"
+				}}
+				defaultCenter={{lat: 37.551229, lng: 126.988205}}
+				defaltzoom={13}
+			>
+				<client.Marker
+					key={1}
+					position={new navermaps.LatLng(37.551229, 126.988205)}
+					animation={1}
+					// onClick={() => {alert('주소 : 서울 강남구 선릉로 627');}}
+				/>
+			</client.NaverMap>
+		);
+	}
 
 	return (
 		<Wrapper>
 			<MapContainer>
-				<RenderAfterNavermapsLoaded
+				<client.RenderAfterNavermapsLoaded
 					ncpClientId={`${API_KEY}`}
 					error={<p>Map Load Error</p>}
 					loading={<p>Map Loading...</p>}
 				>
 					<NaverMapAPI/>
-				</RenderAfterNavermapsLoaded>
+				</client.RenderAfterNavermapsLoaded>
 			</MapContainer>
 		</Wrapper>
 	);
