97d1fb610e0dc2549a127301507f93205384a2db
lnk0302 <lnk0302@users.noreply.github.com>
2021-09-23 12:46:11 +0900

    :fire: 네이버 지도 api 테스트

--- srcs/components/SubMain/SubMain.js
+++ srcs/components/SubMain/SubMain.js
@@ -4,30 +4,16 @@ import Organization from "../about/Orgaization";
 import ProductDrPODI from "../about/ProductDrPODI";
 import ProductShuFooter from "../about/ProductShuFooter";
 import Map from "../about/Map";
-import {useEffect, useState} from "react";
-import dynamic from 'next/dynamic'
-
-// const MapComponent = dynamic(() => import('../about/Map'));
-
-
-// 	loading: () => (<div>Map Loading</div>),
-// 		ssr: false,},
 
 const Wrapper = styled.div`
 `
 
 const SubMain = ({curr}) => {
-
-	// const [load, setLoad] = useState(false);
-	// useEffect(() => {
-	// 	setLoad(true);
-	// }, []);
-
 	return (
 		<Wrapper>
 			{ curr.page === "Profile" &&<Profile/>}
 			{ curr.page === "Organization" &&<Organization/>}
-			{ curr.page === "Map" && load && <Map/>}
+			{ curr.page === "Map" && <Map/>}
 			{ curr.page === "ProductDrPODI" &&<ProductDrPODI/>}
 			{ curr.page === "ProductShuFooter" &&<ProductShuFooter/>}
 		</Wrapper>
